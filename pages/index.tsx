58a86839bdf3309dd0ca1211c0c6eaaa46233cf0
Jaime <jaimemarijke@gmail.com>
2022-09-06 15:36:24 -0700

    Basic first draft styling + placeholder text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,49 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+
+type Service = {
+  title: string;
+  content?: string;
+}
+
+const SERVICES: Service[] = [
+  {
+    title: 'Fractional CTO / VP of Software Engineering',
+  },
+  {
+    title: 'Foundational software team building',
+  },
+  {
+    title: '0-to-1 product strategy and implementation',
+  },
+  {
+    title: '1-to-100+ product scaling',
+  },
+  {
+    title: 'Data pipeline design and and scientific model management',
+  },
+  {
+    title: 'Software strategy coaching and education for non-software founders',
+  }
+]
+
+const LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+const Service = ({ service }: { service: Service }) => {
+  return (
+    <div>
+      <h3 className={styles.serviceTitle}>{service.title}</h3>
+      <p className={styles.serviceContent}>{service.content || LOREM_IPSUM}</p>
+    </div>
+  )
+}
+
+const ServicesList = () => {
+  return <div>{SERVICES.map((service) => <Service service={service} />)}</div>
+}
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,7 +54,7 @@ export default function Home() {
       </Head>
 
       <div className={styles.sidebar}>
-        <Image src='/logo.svg' width='100' height='40' />
+        <Image src='/logo.svg' layout='fixed' width='100' height='40' />
         <nav>
           <ul>
             <li>About</li>
@@ -20,8 +63,10 @@ export default function Home() {
           </ul>
         </nav>
       </div>
-      <main className={styles.main}>
-        Main content
+      <main className={styles.mainContainer}>
+        <div className={styles.mainContent}>
+          <ServicesList />
+        </div>
       </main>
     </div>
   );
