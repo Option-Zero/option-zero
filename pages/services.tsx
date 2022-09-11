6c53bddd90b7465411b843b4a172e5091e66f30f
Jason Curtis <thatneat@users.noreply.github.com>
2022-09-11 15:45:05 +0000

    syntax errors so we can compile in stricter mode

--- pages/services.tsx
+++ pages/services.tsx
@@ -11,15 +11,15 @@ const Services = () => {
   return (<div>
     <h3 className={styles.title}> Software strategy for non-software founders</h3>
     <div className={styles.content}>
-      <p>You’re starting to incorporate software into your hardware-centric company, or ready to scale an existing prototype. What’s a reasonable timeline? Is software always the long pole? When should you build vs buy vs outsource?</p>
-      <p>We’ll be your sounding board as you navigate scaling your software from 0 to 1, or from 1 to 100.</p>
+      <p>You$apos;re starting to incorporate software into your hardware-centric company, or ready to scale an existing prototype. What$apos;s a reasonable timeline? Is software always the long pole? When should you build vs buy vs outsource?</p>
+      <p>We$apos;ll be your sounding board as you navigate scaling your software from 0 to 1, or from 1 to 100.</p>
     </div>
 
     <h3 className={styles.title}>Foundational software team building</h3>
     <div className={styles.content}>
-      <p>You need to hire some software folks, but aren’t sure how to identify the best candidates or compete with FAANG salaries.</p>
-      <p>We'll help you get your first software team off the ground, from designing a hiring pipeline to developing a healthy and efficient team culture.</p>
-      <p>Where we’ve built teams:</p>
+      <p>You need to hire some software folks, but aren$apos;t sure how to identify the best candidates or compete with FAANG salaries.</p>
+      <p>We$apos;ll help you get your first software team off the ground, from designing a hiring pipeline to developing a healthy and efficient team culture.</p>
+      <p>Where we$apos;ve built teams:</p>
       <div className={styles.logos}>
         <EnergySavvyLogo />
         <OsmoLogo />
@@ -29,9 +29,9 @@ const Services = () => {
 
     <h3 className={styles.title}>Data pipelines & scientific model management</h3>
     <div className={styles.content}>
-      <p>You’ve got more data than you can handle. Your models, if you have them, take hours to run.</p>
-      <p>We’ll help make sure your scientists can iterate quickly without losing what you’ve already learned. If you don’t have a science team yet, we’ll run your analysis or build your proof-of-concepts with clean code you can build on.</p>
-      <p>Where we’ve wrangled data:</p>
+      <p>You$apos;ve got more data than you can handle. Your models, if you have them, take hours to run.</p>
+      <p>We$apos;ll help make sure your scientists can iterate quickly without losing what you$apos;ve already learned. If you don$apos;t have a science team yet, we$apos;ll run your analysis or build your proof-of-concepts with clean code you can build on.</p>
+      <p>Where we$apos;ve wrangled data:</p>
       <div className={styles.logos}>
         <MetaLogo />
         <EnergySavvyLogo />
@@ -43,9 +43,9 @@ const Services = () => {
 
     <h3 className={styles.title}>Full stack implementation</h3>
     <div className={styles.content}>
-      <p>You’re not ready to hire full-time software, or your existing employees are busy with mission-critical work.</p>
-      <p>We’ll get your first prototype built or tackle that side project that would accelerate your team or get more customers in the door.</p>
-      <p>Where we’ve delivered:</p>
+      <p>You$apos;re not ready to hire full-time software, or your existing employees are busy with mission-critical work.</p>
+      <p>We$apos;ll get your first prototype built or tackle that side project that would accelerate your team or get more customers in the door.</p>
+      <p>Where we$apos;ve delivered:</p>
       <p>Meta - Microsoft - Convoy - EnergySavvy - Osmo Systems - Qnuru - Synapse - Olin College</p>
       <div className={styles.logos}>
         <MetaLogo />
@@ -82,7 +82,7 @@ const Service = ({ service }: { service: Service }) => {
           <p className={styles.content}>{service.content}</p>
           <ul>
             {(service.bullets || []).map((bullet) => {
-              return <li>{bullet}</li>
+              return <li key={service.title}>{bullet}</li>
             })}
           </ul>
         </Typography>
@@ -92,7 +92,7 @@ const Service = ({ service }: { service: Service }) => {
 }
 
 export const ServicesList = () => {
-  return <div>{SERVICES.map((service) => <Service service={service} />)}</div>
+  return <div>{SERVICES.map((service) => <Service service={service} key={service.title}/>)}</div>
 }
 
 const SERVICES: Service[] = [
@@ -111,7 +111,7 @@ const SERVICES: Service[] = [
   },
   {
     title: '0-to-1 product strategy and implementation',
-    content: 'Rapidly iterate to find product/market fit. Build the cheapest thing possible first (maybe it’s a spreadsheet & a lot of manual phone calls behind the scenes). Don’t worry too much about technical debt yet - more on that later',
+    content: 'Rapidly iterate to find product/market fit. Build the cheapest thing possible first (maybe it\'s a spreadsheet & a lot of manual phone calls behind the scenes). Don’t worry too much about technical debt yet - more on that later',
   },
   {
     title: '1-to-100+ product scaling',
