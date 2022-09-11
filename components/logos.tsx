cec5b6a4364b6eb0c7bc5b79171f311f4c9f8fce
Jaime <jaimemarijke@gmail.com>
2022-09-11 12:39:27 -0700

    smaller logos

--- components/logos.tsx
+++ components/logos.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/future/image';
 
-const logoStyle = { width: '100px', height: '100%', margin: '10px' };
+const logoStyle = { width: '80px', height: '100%', margin: '10px' };
 
 export const EnergySavvyLogo = () => <Image alt='EnergySavvy Logo' src='/EnergySavvyLogo.png' width="0" height="0" sizes="100vw" style={logoStyle} />
 export const OsmoLogo = () => <Image alt='Osmo Logo' src='/OsmoLogo.png' width="0" height="0" sizes="100vw" style={logoStyle} />
