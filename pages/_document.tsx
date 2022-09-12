cf878bb6fbe72986c36f41cb1da55e959777413c
Jaime <jaimemarijke@gmail.com>
2022-09-12 14:24:15 -0700

    Fix build warnings - move font import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:100,200,300,500,600,900" rel="stylesheet" type="text/css"></link>
-        <link href="https://fonts.googleapis.com/css?family=Inconsolata:900" rel="stylesheet" type="text/css"></link>
-        <link rel="stylesheet" type="text/css" href="/FuturaFuturis.css"></link>
+        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:100,200,300,500,600,900&display=optional" rel="stylesheet" type="text/css"></link>
+        <link href="https://fonts.googleapis.com/css?family=Inconsolata:900&display=optional" rel="stylesheet" type="text/css"></link>
       </Head>
       <body>
         <Main />
