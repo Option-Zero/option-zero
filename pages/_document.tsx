4384e3246d00906cbe1999b2798d84cf9f604fb4
Jaime <jaimemarijke@gmail.com>
2022-09-12 15:11:12 -0700

    add 400 weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:100,200,300,500,600,900&display=optional" rel="stylesheet" type="text/css"></link>
+        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:100,200,300,400,500,600,900&display=optional" rel="stylesheet" type="text/css"></link>
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:900&display=optional" rel="stylesheet" type="text/css"></link>
       </Head>
       <body>
