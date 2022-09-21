b934a8ef9626ff43131cc12651f19b2ede4276a9
Jaime <jaimemarijke@gmail.com>
2022-09-21 10:13:52 -0700

    Add social meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:100,200,300,400,500,600,900&display=optional" rel="stylesheet" type="text/css"></link>
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:900&display=optional" rel="stylesheet" type="text/css"></link>
+        <meta property="og:title" content="Option Zero" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://optionzero.co" />
+        <meta property="og:image" content="https://optionzero.co/logo.svg" />
       </Head>
       <body>
         <Main />
