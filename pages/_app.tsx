b7bd9d75792c95bedd182c670cc60ad712ac0747
Jaime <jaimemarijke@gmail.com>
2022-10-10 15:39:11 -0700

    Fix cross-browser issues: button font color, custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import 'normalize.css/normalize.css';  // Sane cross-browser css defaults & resets
+import '../styles/globals.css'; // Import our css second to override normalize
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:600,100,300&display=swap" rel="stylesheet" type="text/css"></link>
+        <link href="https://fonts.googleapis.com/css?family=IBM Plex Sans:600,100,300,500&display=swap" rel="stylesheet" type="text/css"></link>
         {/* <link href="https://fonts.googleapis.com/css?family=Inconsolata:100,500,700,900&display=optional" rel="stylesheet" type="text/css"></link> */}
         <meta property="og:title" content="Option Zero" />
         <meta property="og:type" content="website" />
