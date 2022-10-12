3b1b980433677a2504678bdb101c2c0eb8e1297a
Jaime <jaimemarijke@gmail.com>
2022-10-12 15:29:26 -0700

    Add redirect to google doc portfolio

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ module.exports = {
         permanent: false,
         basePath: false
       },
+      {
+        source: '/portfolio',
+        destination: 'https://docs.google.com/document/d/1Tl0Ly7_f9bOCm0icSl42669aUDiksADPQClUQ-D87dg/edit?usp=sharing',
+        permanent: false,
+        basePath: false
+      },
     ]
   },
 }
